1b32cd391a17420590aebbbc8999e0d0e30043c5
Drone1112 <jamiedunnet@cromwell.school.nz>
2026-01-30 14:44:24 +1300

    chore: scum

--- if_else.py
+++ if_else.py
@@ -3,4 +3,8 @@ name = input("what is you name?")
 print(f"Hello {name}")
 
 if name == "Jamie":
-    print(f"Go directy to jail. Do not pass go")
+    print(f"Go directy to jail. Do not pass go.")
+elif name =="Levi":
+    print(f"Focus on your work.")
+else: 
+    print(f"Collect $200.")
