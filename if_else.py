187a7c755a65aee37072ab1aac840d5d5ffeface
Drone1112 <jamiedunnet@cromwell.school.nz>
2026-01-30 14:38:08 +1300

    chore: new code

---
+++ if_else.py
@@ -0,0 +1,8 @@
+"""How if/else works."""
+
+name = input("what is you name?")
+
+print(f"Hello {name}")
+
+if name == "Jamie":
+    print(f"Go directy to jail. Do not pass go")
--- test.py
+++ test.py
@@ -1,1 +1,31 @@
-print("HELLO WORLD")
+"""This progamrm shows the basics of python coding."""
+
+# Diffrent types of variables to use this year.
+string_variable = "A string is text. Use quotation marks"
+integer_varable = 12
+float_variable = 3.13149
+boolean_variavle = True
+
+# You will need to use function in your code. 
+def hello_world ():
+    print("Hello world")
+    print("Hope you are all doing well")
+
+hello_world()
+
+def maths (ryans_number):
+    print(f"6 plus your number is {ryans_number}")
+
+
+def mathsI (number1, number2):
+    result = number1 * number2
+    print(f"The product of numbers us {result}")
+
+
+mathsI(100, 203)
+
+def greeting(person):
+    print("Hello there {person}")
+
+greeting("bob")
+greeting("Geroge")
