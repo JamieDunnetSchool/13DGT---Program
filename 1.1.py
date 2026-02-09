02dab9af91468493b8a800f39975d6cb13b4f28f
Drone1112 <jamiedunnet@cromwell.school.nz>
2026-02-10 09:54:54 +1300

    chore: i did work

--- 1.1.py
+++ 1.1.py
@@ -17,11 +17,36 @@ Garnish = {
         "4": ("Cheese", 2.50),
     }
 
-
-
 print("Welcome to online samwichs a subway rip off would you like to order?")
 
-bread = input("What bread would you like \n 1 = Wholemeal $1.00 \n 2 = White $0.80 \n 3 = Cheesy White $1.20 \n 4 = Gluten Free $1.40  ")
+def choose_item(category_name, options):
+    print(f"\nChoose your {category_name}:")
 
-print(bread)
-        
+    for key, value in options.items():
+        name, price = value
+        print(f"{key}. {name} - ${price:.2f}")
+
+    while True:
+        choice = input("Enter number: ")
+
+        if choice in options:
+            return options[choice]
+        else:
+            print("Invalid choice. Try again.")
+
+def main():
+    print("Welcome to online samwichs a subway rip off would you like to order?")
+    while True:
+        bread_name, bread_price = choose_item("Bread", Bread)
+        meat_name , meat_price = choose_item("Meat", Meat)
+        garnish_name, garnish_price = choose_item("Garnish", Garnish)
+
+        overall_price = bread_price + meat_price + garnish_price
+
+        print(overall_price)
+        break
+
+
+
+
+main()
