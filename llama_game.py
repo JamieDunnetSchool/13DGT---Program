ac2b4e62c046b151dee15766308ece30e2065e03
Drone1112 <jamiedunnet@cromwell.school.nz>
2026-02-24 10:04:45 +1300

    chore: do stuff

--- llama_game.py
+++ llama_game.py
@@ -16,11 +16,7 @@ blue = (78, 159, 229)
 quit_game = False
 
 clock = pygame.time.Clock()
-snake_x = 490
-snake_y = 350
-snake_x_change = 0
-snake_y_change = 0
-pygame.draw.rect(screen, red, [snake_x, snake_y, 20, 20])
+
 pygame.display.update()
 
 
@@ -30,23 +26,14 @@ while not quit_game:
             quit_game = True
 
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                snake_x_change = -20
-                snake_y_change = 0
-            elif event.key == pygame.K_RIGHT:
-                snake_x_change = 20
-                snake_y_change = 0
-            elif event.key == pygame.K_UP:
+            if event.key == pygame.K_UP:
                 snake_y_change = -20
                 snake_x_change = 0
             elif event.key == pygame.K_DOWN:
                 snake_y_change = 20
                 snake_x_change = 0
 
-    snake_x += snake_x_change
-    snake_y += snake_y_change
     screen.fill(blue)
-    pygame.draw.rect(screen, red, [snake_x, snake_y, 20, 20])
     pygame.display.update()
     clock.tick(10)
 
