c5724476f6160ff26658f3b5cc53c9079ccaab5f
Drone1112 <jamiedunnet@cromwell.school.nz>
2026-02-26 12:49:38 +1300

    chore: remove

--- llama_game.py
+++ llama_game.py
@@ -1,5 +1,6 @@
 import pygame
 import time
+import random
 pygame.init()
 
 screen = pygame.display.set_mode((1000, 500))
@@ -7,6 +8,8 @@ pygame.display.set_caption("Llama game")
 
 game_icon = pygame.image.load('llama_icon.png')
 pygame.display.set_icon(game_icon)
+
+pygame.key.set_repeat()
 
 green = (188, 227, 199)
 black = (0, 0, 0)
@@ -26,13 +29,20 @@ llama_y = 220
 llama_w = 40
 llama_h = 40
 
-cactus_x = 300
+cactus_x = 1200
 cactus_y = 220
 cactus_w = 40
 cactus_h = 40
 
-snake_x_change = 0
-snake_y_change = 0
+llama_x_change = 0
+llama_y_change = 0
+
+touch_ground = False
+jump_lock = False
+
+ground_y = 260 - llama_h
+gravity = 10
+jump_power = -50
 
 while not quit_game:
     for event in pygame.event.get():
@@ -40,22 +50,28 @@ while not quit_game:
             quit_game = True
 
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP  or event.key == pygame.K_SPACE:
-                snake_y_change = -20
-                snake_x_change = 0
-            elif event.key == pygame.K_DOWN:
-                snake_y_change = 20
-                snake_x_change = 0
-        if event.type == pygame.KEYUP:
-            if event.key in (pygame.K_UP, pygame.K_DOWN):
-                snake_y_change = 0
+            if (event.key == pygame.K_UP or event.key == pygame.K_SPACE) and touch_ground == True and jump_lock == False:
+                llama_y_change = jump_power
+                touch_ground = False
+                jump_lock = True
 
-    llama_y += snake_y_change
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP or event.key == pygame.K_SPACE:
+                jump_lock = False
+
+    llama_y += llama_y_change
+    llama_y_change += gravity
+
+    cactus_x -= 10
+
+    if llama_y >= ground_y:
+        llama_y = ground_y
+        llama_y_change = 0
+        touch_ground = True
 
     if llama_y < 0:
         llama_y = 0
-    if llama_y > 260 - llama_h:
-        llama_y = 260 - llama_h
+        llama_y_change = 0
 
     screen.fill(blue)
 
@@ -64,14 +80,13 @@ while not quit_game:
 
     llama = pygame.Rect(llama_x, llama_y, llama_h, llama_w)
     fakellama = pygame.image.load('Llama.png').convert_alpha()
-    resized_llama = pygame.transform.smoothscale(fakellama, [llama_h,llama_w])
+    resized_llama = pygame.transform.smoothscale(fakellama, [llama_h, llama_w])
     screen.blit(resized_llama, llama)
 
     cactus = pygame.Rect(cactus_x, cactus_y, cactus_h, cactus_w)
     fakecac = pygame.image.load('cactus.png').convert_alpha()
-    resized_cactus = pygame.transform.smoothscale(fakecac, [cactus_h,cactus_w])
+    resized_cactus = pygame.transform.smoothscale(fakecac, [cactus_h, cactus_w])
     screen.blit(resized_cactus, cactus)
-
 
     pygame.display.update()
     clock.tick(10)
