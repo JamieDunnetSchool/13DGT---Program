12b9b7db19790b9f96a830c34c77f1fb79de0f5f
Drone1112 <jamiedunnet@cromwell.school.nz>
2026-03-11 09:13:50 +1300

    chore: finsh lamam

--- llama_game.py
+++ llama_game.py
@@ -3,12 +3,13 @@
 import pygame
 import time
 import random
+
 pygame.init()
 
 screen = pygame.display.set_mode((1000, 500))
 pygame.display.set_caption("Llama game")
 
-game_icon = pygame.image.load('llama_icon.png')
+game_icon = pygame.image.load("llama_icon.png")
 pygame.display.set_icon(game_icon)
 
 pygame.key.set_repeat()
@@ -44,6 +45,7 @@ gravity = 5
 jump_power = -50
 score = 0
 pass_score = 0
+final_score = 0
 game_over = False
 game_ending = False
 font = pygame.font.Font("freesansbold.ttf", 50)
@@ -61,22 +63,20 @@ def show_score(x, y):
 
 def load_high_score():
     try:
-        hi_score_file = open("highsocre.txt", 'r')
-    except:
-        hi_score_file = open("highsocre.txt", 'w')
-        hi_score_file.write("0")
-        hi_score_file.close()
-        hi_score_file = open("highsocre.txt", 'r')
-    value = hi_score_file.read().strip()
-    hi_score_file.close()
+        with open("highsocre.txt", "r") as hi_score_file:
+            value = hi_score_file.read().strip()
+    except FileNotFoundError:
+        with open("highsocre.txt", "w") as hi_score_file:
+            hi_score_file.write("0")
+        value = "0"
+
     if value == "":
         return 0
     return int(value)
 
 def save_high_score(value):
-    hi_score_file = open("highsocre.txt", 'w')
-    hi_score_file.write(str(value))
-    hi_score_file.close()
+    with open("highsocre.txt", "w") as hi_score_file:
+        hi_score_file.write(str(value))
 
 high_score = load_high_score()
 
@@ -97,12 +97,17 @@ class cactus:
         screen.blit(resized_cac, cactu)
 
     def hit(self, llama_x, llama_y, llama_w, llama_h):
-        global game_ending
+        global game_ending, final_score, score
         self.cactus_x -= self.speed
         cactus_rect = pygame.Rect(self.cactus_x, self.cactus_y, self.w, self.h)
         llama_rect = pygame.Rect(llama_x, llama_y, llama_w, llama_h)
+
         if llama_rect.colliderect(cactus_rect):
+            if game_ending == False:
+                final_score = int(time.time() - start_time) + pass_score
+                score = final_score
             game_ending = True
+
         if self.cactus_x < -self.w:
             self.cactus_x = 1000 + random.randint(200, 600)
             return self.points
@@ -110,7 +115,7 @@ class cactus:
 
 def reset_game():
     global llama_x, llama_y, llama_y_change, touch_ground, jump_lock
-    global score, pass_score, start_time, game_ending
+    global score, pass_score, start_time, game_ending, final_score
     global cactus1, cactus2, cactus3, cactus_list
 
     llama_x = 100
@@ -121,6 +126,7 @@ def reset_game():
 
     score = 0
     pass_score = 0
+    final_score = 0
     start_time = time.time()
     game_ending = False
 
@@ -137,14 +143,14 @@ cactus_list = [cactus1, cactus2, cactus3]
 while not quit_game:
 
     while game_ending == True:
-        score = int(time.time() - start_time) + pass_score
+        score = final_score
+
         if score > high_score:
             high_score = score
             save_high_score(high_score)
 
         screen.fill(blue)
-        ground_rect = pygame.Rect(0, 500 - ground_size2, ground_size1, 
-                                  ground_size2)
+        ground_rect = pygame.Rect(0, 500 - ground_size2, ground_size1, ground_size2)
         pygame.draw.rect(screen, brown, ground_rect)
         show_score(textX, textY)
         message("You died! Press X to quit, C to play again", black, white)
@@ -195,12 +201,11 @@ while not quit_game:
     score = int(time.time() - start_time) + pass_score
 
     screen.fill(blue)
-    ground_rect = pygame.Rect(0, 500 - ground_size2, ground_size1, 
-                              ground_size2)
+    ground_rect = pygame.Rect(0, 500 - ground_size2, ground_size1, ground_size2)
     pygame.draw.rect(screen, brown, ground_rect)
 
     llama = pygame.Rect(llama_x, llama_y, llama_h, llama_w)
-    fakellama = pygame.image.load('Llama.png').convert_alpha()
+    fakellama = pygame.image.load("Llama.png").convert_alpha()
     resized_llama = pygame.transform.smoothscale(fakellama, [llama_h, llama_w])
     screen.blit(resized_llama, llama)
 
@@ -212,7 +217,7 @@ while not quit_game:
     pygame.display.update()
     clock.tick(FPS)
 
-score = int(time.time() - start_time) + pass_score
+score = final_score if game_ending else int(time.time() - start_time) + pass_score
 if score > high_score:
     high_score = score
 save_high_score(high_score)
